f26159351c8551a20d44dd17b05a37b71efd2410
Samuel Silva <samuellmsl05@gmail.com>
2025-05-13 21:01:39 -0300

    fix: remove unused class

--- internal/domain/ports/output/repositories/user_repository.go
+++
@@ -1,9 +0,0 @@
-package repositories
-
-import (
-    "github.com/samuellalvs/soat_tech_challenge_fast_food/internal/application/dto"
-)
-
-type CustomerRepository interface {
-    CreateCustomer(customer *dto.CreateCustomerDTO) error
-}
