9d7602de60492d01b9ffcf0241f262c0968ec675
Samuel Silva <samuellmsl05@gmail.com>
2025-05-29 20:15:23 -0300

    feat: improve docs

--- internal/application/dto/order_dto.go
+++ internal/application/dto/order_dto.go
@@ -1,9 +1,9 @@
 package dto
 
 type OrderDTO struct {
-	ID         uint64         `json:"id"`
-	CustomerId uint64         `json:"customer_id"`
-	CPF        string         `json:"cpf"`
-	Status     string         `json:"status"`
+	ID         uint64         `json:"id" example:"1"`
+	CustomerId uint64         `json:"customer_id" example:"123"`
+	CPF        string         `json:"cpf" example:"123.456.789-00"`
+	Status     string         `json:"status" example:"pending"`
 	Items      []OrderItemDTO `json:"items"`
 }
--- internal/application/dto/order_item_dto.go
+++ internal/application/dto/order_item_dto.go
@@ -1,9 +1,9 @@
 package dto
 
 type OrderItemDTO struct {
-	ID        uint64  `json:"id"`
-	OrderID   uint64  `json:"order_id"`
-	ProductId uint64  `json:"product_id"`
-	Quantity  uint64  `json:"quantity"`
-	Price     float32 `json:"price"`
+	ID        uint64  `json:"id" example:"1"`
+	OrderID   uint64  `json:"order_id" example:"100"`
+	ProductId uint64  `json:"product_id" example:"200"`
+	Quantity  uint64  `json:"quantity" example:"2"`
+	Price     float32 `json:"price" example:"19.99"`
 }
